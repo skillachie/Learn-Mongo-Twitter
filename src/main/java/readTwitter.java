a033c0e4c1018ef29b33c78a46596cf0365299d7
Dwayne Campbell <dwaynecampbell13@gmail.com>
2013-07-20 18:30:51 -0400

    stubbed code

---
+++ src/main/java/readTwitter.java
@@ -0,0 +1,15 @@
+//import twitter4j.AccountSettings;
+
+public class readTwitter {
+
+
+
+   //Complete code here to read from Twitter API  for Keyword Taekwondo
+    //and print
+
+    //Look at the structure of the json document  returned from Twitter
+    //And see if any other attributes are interested
+
+
+
+}
