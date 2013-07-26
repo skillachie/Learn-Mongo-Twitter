9dd6f031a1d0de7ceb67bb98b8a73bcc47f50c1d
Wardell Samotshozo <wardell.samotshozo@gmail.com>
2013-07-25 22:51:37 -0400

    Program getts taekwondo tweets

---
+++ src/readTwitter.java
@@ -0,0 +1,35 @@
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+//import twitter4j.AccountSettings;
+
+public class readTwitter {
+	
+   //Complete code here to read from Twitter API  for Keyword Taekwondo
+    //and print
+	public static void main(String args[]) throws TwitterException{
+    ConfigurationBuilder cb = new ConfigurationBuilder();
+    cb.setDebugEnabled(true)
+          .setOAuthConsumerKey("OlkcSblmsWdFiUlxmsbzXA")
+          .setOAuthConsumerSecret("KTN9oHHY0nxVND8mbwEB4OGU44q0jDVdjqCbBbCy9vM")
+          .setOAuthAccessToken("1621819165-4smTxoDwwgvVozYepwHFFVmoLLoW0m2BcSgGfG6")
+          .setOAuthAccessTokenSecret("oBlDSqT1oc8TujVTgzm9StoF9kfMPmsAqmVKY7Rm0DY");
+    TwitterFactory tf = new TwitterFactory(cb.build());
+    Twitter twitter = tf.getInstance();
+    Query query = new Query("taekwondo");
+    QueryResult result = twitter.search(query);
+    for (Status status : result.getTweets()) {
+        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+    }
+	}
+    //Look at the structure of the json document  returned from Twitter
+    //And see if any other attributes are interested
+
+
+
+}
