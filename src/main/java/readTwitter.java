f3668e1a665bd9ca3136da9bb9622a93c18f4df8
Wardell Samotshozo <wardell.samotshozo@gmail.com>
2013-08-20 15:20:33 -0400

    actual changes

--- src/main/java/readTwitter.java
+++ src/main/java/readTwitter.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -10,8 +12,11 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class readTwitter {
 	
-   //Complete code here to read from Twitter API  for Keyword Taekwondo
+	   //Complete code here to read from Twitter API  for Keyword Taekwondo
     //and print
+	static int n = 20;
+	static String keyword = "startup";
+	static String access = "taekwondo";
 	public static void main(String args[]) throws TwitterException{
     ConfigurationBuilder cb = new ConfigurationBuilder();
     cb.setDebugEnabled(true)
@@ -21,15 +26,24 @@ public class readTwitter {
           .setOAuthAccessTokenSecret("oBlDSqT1oc8TujVTgzm9StoF9kfMPmsAqmVKY7Rm0DY");
     TwitterFactory tf = new TwitterFactory(cb.build());
     Twitter twitter = tf.getInstance();
-    Query query = new Query("taekwondo");
+    Query query = new Query(keyword);
+    query.setLang("en");
+    query.setCount(n);
     QueryResult result = twitter.search(query);
     for (Status status : result.getTweets()) {
-        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText()
+        		+ " [" +status.getGeoLocation() +"]");
+    }
+	System.out.println("\n\n************\n\n");
+	query = new Query(access);
+	result = twitter.search(query);
+    for (Status status : result.getTweets()) {
+        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText()
+        		+ " [" +status.getGeoLocation() +"]");
     }
 	}
     //Look at the structure of the json document  returned from Twitter
     //And see if any other attributes are interested
-
 
 
 }
